287d09f46bb4fd05d8d0dc297b6dbc8f13d5c019
benibecan <benibecan@naver.com>
2023-08-22 17:58:10 +0900

    0822

--- web-portfolio/0822-samyangfood-redesign/js/btn.js
+++ web-portfolio/0822-samyangfood-redesign/js/btn.js
@@ -1,41 +1,33 @@
-// ▼ product클릭 이벤트
-/*
 let prod = document.querySelectorAll('.product_box>div>div');
+
 for (let i = 0; i < 5; i++) {
     prod[i].style.opacity = '0';
 }
 prod[0].style.opacity = '1';
 
-let prodMenu = document.querySelectorAll('.product_menu>div>ul');
+let prodMenu = document.querySelectorAll('.product_icon a');
 prodMenu.forEach(function (item, keys) {
+    console.log('item :' + item + '\n' + 'keys : ' + keys);
     item.onclick = function (e) {
-        let j = Array.from('prodMenu').indexOf(e.target);
+        let j = Array.from(prodMenu).indexOf(e.target);
         console.log(j);
+        function others(item) {
+            return item !== j;
+        }
+        let otherItems = Array.from(prod).filter(others);
+        otherItems.forEach(function (item) {
+            item.style.opacity = '0';
+        });
+        prod[j].style.opacity = '1';
     };
 });
-*/
+// ▲ product클릭 이벤트
 
-/*
-.ramenContents ramenBtn
-.snackContents snackBtn
-.dairyContents dairyBtn
-.souceContents souceBtn
-.frozenContents frozenBtn
-*/
-var bDisplay = true;
-function doDisplay() {
-    var con = document.getElementById('myDIV');
-    if (con.style.display == 'none') {
-        con.style.display = 'block';
-    } else {
-        con.style.display = 'none';
-    }
-}
-// ▼ header nav 삼양식품 클릭 이벤트
 document.querySelector('.switch_btn__left').addEventListener('click', (c) => {
     window.location = 'https://brand.naver.com/syfoodshop';
 });
-// ▼ sns클릭 이벤트
+// ▲ header nav 삼양식품 클릭 이벤트
+
 document.querySelector('.sns_1').addEventListener('click', (c) => {
     window.location = 'https://blog.naver.com/samyangfoods';
 });
@@ -48,3 +40,4 @@ document.querySelector('.sns_3').addEventListener('click', (c) => {
 document.querySelector('.sns_4').addEventListener('click', (c) => {
     window.location = 'https://twitter.com/i/flow/login?redirect_after_login=%2Fsamyangfoods';
 });
+// ▲ sns클릭 이벤트
